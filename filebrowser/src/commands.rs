86607433eb99d0006432db771fc5fdce22648664
Dylan Bulfin <dylanbulfin@gmail.com>
2024-05-18 09:02:14 -0400

    Adding color coding, rm, fix ls formatting

--- filebrowser/src/commands.rs
+++ filebrowser/src/commands.rs
@@ -1,25 +1,44 @@
-use std::{fs, path::PathBuf};
+use std::{fs, path::{Path, PathBuf}};
 
 use crate::{paths::get_canon_path, types::State};
 
+use colored::{ColoredString, Colorize};
+
 fn get_file_list(dir: &PathBuf) -> Vec<String> {
-    let paths = fs::read_dir(dir).unwrap();
+    let unsorted_paths = fs::read_dir(dir).unwrap();
+
+    let mut paths = unsorted_paths
+        .map(|x| String::from(x.unwrap().path().to_str().unwrap()))
+        .collect::<Vec<String>>();
+    paths.sort();
 
     paths
-        .map(|x| String::from(x.unwrap().path().to_str().unwrap()))
-        .collect()
+}
+
+fn colorize_file(path_str: &String) -> ColoredString {
+    if PathBuf::from(path_str.as_str()).is_dir() {
+        path_str.blue().bold()
+    } else {
+        path_str.bright_cyan()
+    }
 }
 
 fn print_file_list(files: Vec<String>) {
-    println!("");
+    if files.is_empty() {
+        return;
+    }
+
+    let max_len = files.iter().map(String::len).max().unwrap();
+
     for c in files.chunks(2) {
         if c.len() == 1 {
-            println!("{}", c[0])
+            println!("{}", colorize_file(&c[0]))
         } else {
-            println!("{}    {}", c[0], c[1])
+            let pad = max_len - c[0].len();
+            print!("{}    ", colorize_file(&c[0]));
+            print!("{:indent$}{}\n", "", colorize_file(&c[1]), indent = pad);
         }
     }
-    println!("");
 }
 
 pub fn change_directory(tail: Vec<String>, state: &mut State) -> Result<(), String> {
@@ -39,7 +58,42 @@ pub fn change_directory(tail: Vec<String>, state: &mut State) -> Result<(), Stri
     Ok(())
 }
 
-pub fn quit(tail: Vec<String>, state: &mut State, quit_flag: &mut bool) -> Result<(), String> {
+pub fn list_files(tail: Vec<String>, state: &State) -> Result<(), String> {
+    if tail.len() != 0 {
+        return Err(String::from("This command requires exactly 0 arguments"));
+    }
+
+    print_file_list(get_file_list(state.get_cwd()));
+
+    Ok(())
+}
+
+pub fn remove_files(tail: Vec<String>, state: &State) -> Result<(), String> {
+    let mut remove_list: Vec<(PathBuf,String)> = Vec::new();
+    for f in tail {
+        if let Ok(path) = get_canon_path(f.as_str(), state) {
+            if path.is_dir()  {
+                return Err(String::from("Only file deletion supported currently, aborting"));
+            }
+            
+            remove_list.push((path, f));
+        } else {
+            return Err(String::from("Error getting full path. File likely doesn't exist"));
+        }
+    }
+
+    for (f, name) in remove_list{
+        if let Ok(()) = fs::remove_file(f) {
+            println!("Successfully removed file {}", name);
+        } else {
+            return Err(format!("Unable to delete file {}", name));
+        };
+    }
+
+    Ok(())
+}
+
+pub fn quit(tail: Vec<String>, quit_flag: &mut bool) -> Result<(), String> {
     if tail.len() != 0 {
         return Err(String::from("This command requires exactly 0 arguments"));
     }
--- filebrowser/src/main.rs
+++ filebrowser/src/main.rs
@@ -1,13 +1,12 @@
-mod types;
-mod paths;
-mod commands; 
+mod commands;
 mod parser;
+mod paths;
+mod types;
 use std::io::{self, BufRead, Write};
 
-
+use colored::Colorize;
 use parser::parse_command;
 use parser::Command;
-use paths::get_canon_path;
 use types::State;
 
 fn main() -> io::Result<()> {
@@ -18,7 +17,7 @@ fn main() -> io::Result<()> {
 
     loop {
         //print a prompt
-        print!("{}> ", &state.get_cwd_str());
+        print!("{}{} ", &state.get_cwd_str().green(), ">".green());
 
         io::stdout().flush()?;
 
@@ -31,13 +30,19 @@ fn main() -> io::Result<()> {
 
         let command = parse_command(buf.as_str());
 
+        println!("");
+
         let res = match command {
             Command::CD(tail) => commands::change_directory(tail, &mut state),
-            Command::QT(tail) => commands::quit(tail, &mut state, &mut quit_flag),
-            _ => Ok(()) 
+            Command::QT(tail) => commands::quit(tail, &mut quit_flag),
+            Command::LS(tail) => commands::list_files(tail, &state),
+            Command::RM(tail) => commands::remove_files(tail, &state),
+            _ => Ok(()),
         };
 
-        if let Err(msg)= res {
+        println!("");
+
+        if let Err(msg) = res {
             eprintln!("{}", msg);
         }
 
